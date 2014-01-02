a1510ba3ac1a0aeda16ce05d03ee7a7d832697e5
cyril agosta <cyril.agosta@gmail.com>
2014-01-02 16:08:05 +0100

    modify bower.json main and add app.js for bower-require compatibility

--- app/templates/_app.js
+++ app/templates/_app.js
@@ -1,7 +1,7 @@
 // just an alias to the main to have bower-requirejs working
 
 define( [
-    './<%= config.name.raw %>'
+    '<%= config.name.raw %>/<%= config.name.raw %>'
  ], function( <%= config.name.camel %> ) {
 
     return <%= config.name.camel %>
